624cd52a0d9d9363918f61852fc130586a7c1516
Renan Siravegna <renan.skz@gmail.com>
2014-07-24 00:22:33 -0400

    Mudando configurações da porta

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var coffee = require('coffee-script/register');
 var app = require('./app/app.coffee');
 
-process.env.port = process.env.port || 3000
+var port = process.env.PORT || 3000;
 
-app.application.listen(process.env.port, function() {
-  console.log('Digicom iniciada na porta ' + process.env.port);
+app.application.listen(port, function() {
+  console.log('Digicom iniciada na porta ' + port);
 });
