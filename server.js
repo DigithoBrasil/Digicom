f9ded121e08439613bb48a0fcd79f740f91074ba
Renan Siravegna <renan.skz@gmail.com>
2014-07-24 00:24:06 -0400

    Tentando uma exibição qualquer no azure...

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-var coffee = require('coffee-script/register');
+/*var coffee = require('coffee-script/register');
 var app = require('./app/app.coffee');
 
 var port = process.env.PORT || 3000;
 
 app.application.listen(port, function() {
   console.log('Digicom iniciada na porta ' + port);
-});
+});*/
+
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
