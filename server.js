57b5c80b16afea818a4f44f7f913757d94aabaa2
Renan Siravegna <renan.skz@gmail.com>
2014-07-24 00:18:05 -0400

    Ajustando a porta de utilização da aplicação e mudando o server.js de local

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var coffee = require('coffee-script/register');
-var app = require('./app.coffee');
+var app = require('./app/app.coffee');
+
+process.env.port = process.env.port || 3000
 
 app.application.listen(process.env.port, function() {
   console.log('Digicom iniciada na porta ' + process.env.port);
