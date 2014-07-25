b0ffd4d6ee49a290c34bfe78c92c296dadb27d59
Renan Siravegna <renan_sm@msn.com>
2014-07-25 00:33:27 -0400

    Mudando toda a estrutura para teste de deploy

--- app/models/natureza.coffee
+++
@@ -1,3 +0,0 @@
-module.exports = class Natureza
-	@receita: 'Receita'
-	@despesa: 'Despesa'
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var coffee = require('coffee-script/register');
-var app = require('./app/app');
+var app = require('./app');
 
 app.start();
